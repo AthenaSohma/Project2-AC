7e32ae0f51b2a608c18421f0871d9cb06793ac14
Athena Cannon <eyelessathena@gmail.com>
2024-03-13 18:15:59 -0400

    Added Wife-Kitchen

---
+++ 6_Wife/script.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    function keyboard(event){
+        console.log ("keyboard pressed")
+
+        if(event.key == "1"){
+            window.location.replace("../3_Frankenstein/index.html")
+        }else if (event.key == "2"){
+            window.location.replace("../MainFloor/index.html")
+        }
+    }
+
+    $(window).keypress(keyboard);
+
+    function openNote(){
+        $(".body-text").toggleClass("hidden")
+    }
+
+    $(".note").click(openNote);    
+    $(".body-text").click(openNote);
+
+    function openTitle(){
+        $(".header-text").toggleClass("hidden")
+    }
+
+    $(".note").click(openTitle);    
+    $(".header-text").click(openTitle);
+}
