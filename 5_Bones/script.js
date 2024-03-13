06328f874dd929fb707fbda10adbc7b38fd01845
Athena Cannon <eyelessathena@gmail.com>
2024-03-13 18:28:10 -0400

    Added Bones-Attic

---
+++ 5_Bones/script.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    function keyboard(event){
+        console.log ("keyboard pressed")
+
+        if(event.key == "1"){
+            window.location.replace("../Upstairs/index.html")
+        }
+    }
+
+    $(window).keypress(keyboard);
+    
+    function openNote(){
+        $(".body-text").toggleClass("hidden")
+    }
+
+    $(".note").click(openNote);    
+    $(".body-text").click(openNote);
+
+    function openTitle(){
+        $(".header-text").toggleClass("hidden")
+    }
+
+    $(".note").click(openTitle);    
+    $(".header-text").click(openTitle);
+}
