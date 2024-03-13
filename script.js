3645f13de2e131a4cb15654bb917d49961ec2133
Athena Cannon <eyelessathena@gmail.com>
2024-03-13 16:18:48 -0400

    Script Edits

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ window.onload = function(){
         console.log ("keyboard pressed")
 
         if(event.key == "1"){
-            window.location.replace("../MainFloor/index.html")
+            window.location.replace("MainFloor/index.html")
         }else if (event.key == "2"){
-            window.location.replace("../Corn_LR/index.html")
+            window.location.replace("Corn_LR/index.html")
         }else if (event.key == "3"){
-            window.location.replace("../8_Lacrimosa/index.html")
+            window.location.replace("8_Lacrimosa/index.html")
         }else if (event.key == "4"){
-            window.location.replace("../2_Norwood/index.html")
+            window.location.replace("2_Norwood/index.html")
         }
     }
 
