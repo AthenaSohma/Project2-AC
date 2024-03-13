85137cf9a22622450e04ab9cff49987eccacd6be
Athena Cannon <72507631+AthenaSohma@users.noreply.github.com>
2024-03-12 20:23:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    function keyboard(event){
+        console.log ("keyboard pressed")
+
+        if(event.key == "1"){
+            window.location.replace("MainFloor/index.html")
+        }else if (event.key == "2"){
+            window.location.replace("Corn_LR/index.html")
+        }else if (event.key == "3"){
+            window.location.replace("8_Lacrimosa/index.html")
+        }else if (event.key == "4"){
+            window.location.replace("2_Norwood/index.html")
+        }
+    }
+
+    $(window).keypress(keyboard);
+}
