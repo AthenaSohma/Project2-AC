642e32905cb99387b981a7ebee632d29c47501aa
Athena Cannon <eyelessathena@gmail.com>
2024-03-13 18:35:03 -0400

    Added Redacted

---
+++ REDACTED/script.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    function keyboard(event){
+        console.log ("keyboard pressed")
+
+        if(event.key == "1"){
+            window.location.replace("../Upstairs/index.html")
+        }
+    }
+
+    $(window).keypress(keyboard);
+}
